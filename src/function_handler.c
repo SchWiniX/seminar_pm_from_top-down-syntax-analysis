49b24b81c0820189dac6aac93b91d34e15f720ca
SchWiniX <schwinix@proton.me>
2025-03-06 13:00:14 +0100

    almost works now

--- src/function_handler.c
+++ src/function_handler.c
@@ -7,14 +7,14 @@ int uppercase_char_to_index(char C) {
 	return (int) C - 65;
 }
 
-void process_remainder(rule* r, char* rule_remainder) {
-	int indx = 2;
+void process_remainder(rule* r, char* rule_remainder, uint32_t start_from) {
+	int indx = start_from;
 	char curr;
 	while ((curr = *(rule_remainder++)) != '\0') {
 		if (curr == ' ') continue;
 		r->remainder[indx++] = curr;
 	}
-	r->remainder_count = indx - 1;
+	r->remainder_count = indx;
 }
 
 rule* create_rule(char name, char* rule_input) {
@@ -28,9 +28,23 @@ rule* create_rule(char name, char* rule_input) {
 	new_rule->singletons = (char*) malloc(512);
 	new_rule->remainder = (char*) malloc(512);
 
-	int32_t indx = 0;
+
+	int32_t indx = -1;
 	char curr;
 	char last = '|';
+
+	char* rule_input_2 = rule_input;
+	while ((curr = *(rule_input_2++)) != '\0') {
+		if (curr == '|') {
+			indx = 0;
+			break;
+		}
+	}
+	if (indx == -1) {
+		process_remainder(new_rule, rule_input, 0);
+		new_rule->singleton_count = 0;
+		return new_rule;
+	}
 
 	while ((curr = *(rule_input++)) != '\0') {
 		if (indx >= 512) return NULL;
@@ -40,7 +54,7 @@ rule* create_rule(char name, char* rule_input) {
 			//swap from reading Y_1...Y_m to reading Z_1...Z_m
 			new_rule->remainder[0] = last;
 			new_rule->remainder[1] = curr;
-			process_remainder(new_rule, rule_input);
+			process_remainder(new_rule, rule_input, 2);
 			break;
 		} else if (curr == '|' && last == '|') new_rule->singletons[indx++] = '\0';
 		else if (curr != '|' && last == '|') new_rule->singletons[indx++] = curr;
@@ -52,58 +66,79 @@ rule* create_rule(char name, char* rule_input) {
 	return new_rule;
 }
 
-int apply_rule(char* input_string, uint8_t rule_index, rule* grammer[]) {
+int apply_rule(char* input_string, char name_char, rule* grammer[]) {
+	uint32_t rule_index = uppercase_char_to_index(name_char);
 	static int h = 0;
 	int h_old = h;
 
+	printf("Call %c (h = %d)\n", name_char, h);
 	rule* curr_rule = grammer[rule_index];
+
+
 	for(int i = 0; i < curr_rule->singleton_count; i++) {
 		int pot_index = uppercase_char_to_index(curr_rule->singletons[i]);
 
-		if (0 <= pot_index || pot_index <= 25) {
-			if(apply_rule(input_string, pot_index, grammer)) return 1;
+		if (0 <= pot_index && pot_index <= 25) {
+			if(apply_rule(input_string, curr_rule->singletons[i], grammer)) {
+				printf("%c returned True (h = %d)\n", name_char, h);
+				return 1;
+			}
 			h = h_old;
 			continue;
 		}
 
-		if(input_string[h++] == curr_rule->singletons[i]) return 1;
-		h = h_old;
-	}
-
-	for(int i = 0; i < curr_rule->remainder_count - 1; i++) {
-		int pot_index = uppercase_char_to_index(curr_rule->singletons[i]);
-
-		if (0 <= pot_index || pot_index <= 25) {
-			if(!apply_rule(input_string, pot_index, grammer)) {
-				h = h_old;
-				return 0;
-			}
-			h++;
-			continue;
-		}
-
-		if(input_string[h++] != curr_rule->singletons[i]) {
-			h = h_old;
-			return 0;
-		}
-		h++;
-	}
-
-	int pot_index = uppercase_char_to_index(curr_rule->singletons[curr_rule->remainder_count]);
-
-		if (0 <= pot_index || pot_index <= 25) {
-			if(!apply_rule(input_string, pot_index, grammer)) {
-				h = h_old;
-				return 0;
-			}
+		if(input_string[h] == curr_rule->singletons[i]) {
+			printf("singletons: %c: %d matched with input_string[h]: %c\n", curr_rule->singletons[i], pot_index, input_string[h]);
+			printf("%c returned True (h = %d)\n", name_char, h);
 			h++;
 			return 1;
 		}
+		printf("singletons: %c: %d NOT matched with input_string[h]: %c\n", curr_rule->singletons[i], pot_index, input_string[h]);
+		h = h_old;
+	}
 
-		if(input_string[h++] != curr_rule->singletons[curr_rule->remainder_count]) {
+
+	for(int i = 0; i < curr_rule->remainder_count - 1; i++) {
+		int pot_index = uppercase_char_to_index(curr_rule->remainder[i]);
+
+		if (0 <= pot_index && pot_index <= 25) {
+			if(!apply_rule(input_string, curr_rule->remainder[i], grammer)) {
+				h = h_old;
+				printf("%c returned False (h = %d)\n", name_char, h);
+				return 0;
+			}
+			continue;
+		}
+
+		if(input_string[h] != curr_rule->remainder[i]) {
+			printf("remainder: %c: %d NOT matched with input_string[h]: %c\n", curr_rule->remainder[i], pot_index, input_string[h]);
+			printf("%c returned False (h = %d)\n", name_char, h);
 			h = h_old;
 			return 0;
 		}
+		printf("remainder: %c: %d matched with input_string[h]: %c\n", curr_rule->remainder[i], pot_index, input_string[h]);
+		h++;
+	}
+
+
+	int pot_index = uppercase_char_to_index(curr_rule->remainder[curr_rule->remainder_count]);
+
+		if (0 <= pot_index && pot_index <= 25) {
+			if(!apply_rule(input_string, curr_rule->remainder[curr_rule->remainder_count], grammer)) {
+				printf("%c returned False (h = %d)\n", name_char, h);
+				h = h_old;
+				return 0;
+			}
+			return 1;
+		}
+
+		if(input_string[h] != curr_rule->remainder[curr_rule->remainder_count]) {
+		printf("remainder: %c: %d NOT matched with input_string[h]: %c\n", curr_rule->remainder[curr_rule->remainder_count], pot_index, input_string[h]);
+			printf("%c returned False (h = %d)\n", name_char, h);
+			h = h_old;
+			return 0;
+		}
+		printf("remainder: %c: %d matched with input_string[h]: %c\n", curr_rule->remainder[curr_rule->remainder_count], pot_index, input_string[h]);
 		h++;
 		return 1;
 }
--- tests/function_handler_tests.c
+++ tests/function_handler_tests.c
@@ -2,13 +2,38 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	char* t1 = "A | b|C |d | (a+b)";
-	rule* r = create_rule('X', t1);
-	printf("%c -> ", r->name);
+	char* t1 = "R | (B)";
+	char* t2 = "E=E";
+	char* t3 = "a | b | (E+E)";
+	rule* r[26];
+	for(int i = 0; i < 26; i++) r[i] = NULL;
+	printf(
+			"B: %d, R: %d, E: %d\n",
+			uppercase_char_to_index('B'),
+			uppercase_char_to_index('R'),
+			uppercase_char_to_index('E')
+		);
+	r[uppercase_char_to_index('B')] = create_rule('B', t1);
+	r[uppercase_char_to_index('R')] = create_rule('R', t2);
+	r[uppercase_char_to_index('E')] = create_rule('E', t3);
 
-	for(int i = 0; i < r->singleton_count; i++) {
-		printf("| %c ", r->singeltons[i]);
+	for(int i = 0; i < r[uppercase_char_to_index('R')]->remainder_count; i++)
+		printf("'%c'\n", r[uppercase_char_to_index('R')]->remainder[i]);
+
+	for(int j = 0; j < 26; j++) {
+		if (r[j] == NULL) continue;
+		printf("%c -> ", r[j]->name);
+
+		if (r[j]->singleton_count != 0)
+			printf("%c ", r[j]->singletons[0]);
+		for(int i = 1; i < r[j]->singleton_count; i++) {
+			printf("| %c ", r[j]->singletons[i]);
+		}
+		if (r[j]->singleton_count != 0) printf("| %s\n", r[j]->remainder);
+		else printf("%s\n", r[j]->remainder);
+		
 	}
-	printf("| %s\n", r->str);
+
+	printf("%d\n", apply_rule("(a=(b+a))", 'B', r));
 	return 0;
 }
